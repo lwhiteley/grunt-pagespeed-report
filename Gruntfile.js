104dca54b9ae2a82edc611571980d2bfc0944f7b
Layton Whiteley <layton.whiteley@gmail.com>
2014-09-08 09:45:48 -0400

    split e2e from unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,8 @@ module.exports = function(grunt) {
 
     // Unit tests.
     nodeunit: {
-      tests: ['test/**/*.test.js']
+      e2e: ['test/e2e/**/*.test.js'],
+      unit: ['test/unit/**/*.test.js']
     }
 
   });
@@ -71,7 +72,9 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'pagespeedReport', 'nodeunit']);
+  grunt.registerTask('test:e2e', ['clean', 'pagespeedReport', 'nodeunit:e2e']);
+  grunt.registerTask('test:unit', ['nodeunit:unit']);
+  grunt.registerTask('test', ['test:unit', 'test:e2e']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- test/e2e/pagespeed_report.test.js
+++ test/e2e/pagespeed_report.test.js
@@ -22,27 +22,27 @@ var grunt = require('grunt');
     test.ifError(value)
 */
 
-// exports.pagespeed_report = {
-//   setUp: function(done) {
-//     // setup here if necessary
-//     done();
-//   },
-//   default_options: function(test) {
-//     test.expect(1);
-//
-//     var actual = grunt.file.read('tmp/default_options');
-//     var expected = grunt.file.read('test/expected/default_options');
-//     test.equal(actual, expected, 'should describe what the default behavior is.');
-//
-//     test.done();
-//   },
-//   custom_options: function(test) {
-//     test.expect(1);
-//
-//     var actual = grunt.file.read('tmp/custom_options');
-//     var expected = grunt.file.read('test/expected/custom_options');
-//     test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-//
-//     test.done();
-//   },
-// };
+exports.pagespeed_report = {
+  setUp: function(done) {
+    // setup here if necessary
+    done();
+  },
+  default_options: function(test) {
+    test.expect(1);
+
+    // var actual = grunt.file.read('tmp/default_options');
+    // var expected = grunt.file.read('test/expected/default_options');
+    test.ok(true,  'should describe what the default behavior is.');
+
+    test.done();
+  },
+  // custom_options: function(test) {
+  //   test.expect(1);
+  //
+  //   var actual = grunt.file.read('tmp/custom_options');
+  //   var expected = grunt.file.read('test/expected/custom_options');
+  //   test.equal(true, true, 'should describe what the custom option(s) behavior is.');
+  //
+  //   test.done();
+  // },
+};
