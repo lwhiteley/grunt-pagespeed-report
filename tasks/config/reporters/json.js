5ce6b3ddf40bcf54b34bd1f3be02184abebf3329
Layton Whiteley <layton.whiteley@gmail.com>
2014-09-28 18:52:59 -0400

    update json reporter

--- tasks/config/reporters/json.js
+++ tasks/config/reporters/json.js
@@ -1,6 +1,6 @@
 'use strict';
-var jsoni = require('jsoni'),
-    _ = require('lodash'),
+var _ = require('lodash'),
+    jsonFormat = require('json-format'),
     grunt = require('grunt');
 
 var util = require('../util/util');
@@ -8,8 +8,8 @@ var util = require('../util/util');
 module.exports = {
   json: function(data, options, generatedContent, callback){
     if(_.isString(options.dest)){
-      grunt.file.write(options.dest + '/json/' + generatedContent.task + '.json', jsoni.stringify(generatedContent));
-
+      grunt.file.write(options.dest + '/json/' + generatedContent.task + '.json', jsonFormat(generatedContent));
+      grunt.file.write(options.dest + '/json/content/' + data.uuid + '.json', jsonFormat(data));
     }
     callback(generatedContent);
   }
